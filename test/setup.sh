1df69ca11c31889ebc1e41ba8026a4cc2f9eed31
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-02-04 00:15:00 +0100

    Add github workflow

---
+++ test/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+FOLDER_SOURCE="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
+FOLDER_PLUGIN="$(readlink -m "${FOLDER_SOURCE}/..")"
+
+symlink() {
+    ln -sfv "$@"
+}
+
+append_package_load()
+{
+    cat <<EOF >>"${1}"
+set runtimepath^=${FOLDER_PLUGIN} runtimepath+=${FOLDER_PLUGIN}/after
+EOF
+}
+
+# check if nvim exists
+if [ "${EDITOR}" = "nvim" ]; then
+    mkdir -p "${HOME}/.config/nvim"
+    append_package_load "${HOME}/.config/nvim/init.vim"
+else
+    mkdir -p "${HOME}/.vim"
+    append_package_load "${HOME}/.vim/vimrc"
+fi
+
